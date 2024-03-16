7290bcc8943a214cf395442b96fc742cb035647b
KarigenBrown <karigenbrown@gmail.com>
2024-03-16 12:48:45 +0800

    init project

---
+++ weg-admin/src/main/java/edu/bistu/Admin.java
@@ -0,0 +1,11 @@
+package edu.bistu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Admin {
+    public static void main(String[] args) {
+        SpringApplication.run(Admin.class, args);
+    }
+}
