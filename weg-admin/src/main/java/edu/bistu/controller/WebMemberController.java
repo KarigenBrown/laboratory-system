b49353303ebe731f85eabc4e7ae89dd8e064eff7
KarigenBrown <karigenbrown@gmail.com>
2024-04-04 16:58:47 +0800

    后台系统后端12

--- weg-admin/src/main/java/edu/bistu/controller/WebMemberController.java
+++ weg-admin/src/main/java/edu/bistu/controller/WebMemberController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -36,11 +37,10 @@ public class WebMemberController {
     }
 
     @PostMapping("/photo")
-    public Response<Map<String, String>> uploadPhoto(@RequestHeader("number") String number, @RequestPart("photo") MultipartFile file) {
-        int index = file.getOriginalFilename().lastIndexOf('.');
-        String suffix = file.getOriginalFilename().substring(index);
-        String url = minioUtils.upload("web", "memberPhoto/" + number + suffix, file);
-        index = url.lastIndexOf('?');
+    public Response<Map<String, String>> uploadPhoto(@RequestParam("photoName") String photoName,
+                                                     @RequestPart("photo") MultipartFile file) {
+        String url = minioUtils.upload("web", "memberPhoto/" + photoName, file);
+        int index = url.lastIndexOf('?');
         url = url.substring(0, index);
         return Response.ok(Map.of("photoUrl", url));
     }
@@ -49,6 +49,37 @@ public class WebMemberController {
     public Response<Object> putMemberById(@RequestBody WebMember member) {
         webMemberService.updateById(member);
         return Response.ok();
+    }
+
+    //
+
+    @GetMapping("/all")
+    public Response<List<WebMember>> getAllMembers() {
+        return Response.ok(webMemberService.list());
+    }
+
+    @GetMapping("/name/{name}")
+    public Response<List<WebMember>> getMembersByName(@PathVariable("name") String name) {
+        return Response.ok(webMemberService.lambdaQuery().like(WebMember::getName, name).list());
+    }
+
+    @GetMapping("/identity/{identity}")
+    public Response<List<WebMember>> getMembersByIdentity(@PathVariable("identity") String identity) {
+        return Response.ok(webMemberService.lambdaQuery().eq(WebMember::getIdentity, identity).list());
+    }
+
+    @DeleteMapping("/{id}")
+    public Response<Object> deleteMemberById(@PathVariable("id") Integer id) {
+        webMemberService.removeById(id);
+        return Response.ok();
+    }
+
+    @PostMapping
+    public Response<Object> postMember(@RequestBody WebMember member) {
+        member.setPhotoUrl("");
+        webMemberService.save(member);
+        Integer id = webMemberService.lambdaQuery().orderByDesc(WebMember::getId).list().get(0).getId();
+        return Response.ok(Map.of("id", id));
     }
 }
 
